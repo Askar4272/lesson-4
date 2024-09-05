c89847557b0ac7560949b5172516eea125d3b1e1
Askar4272 <askar4272@gmail.com>
2024-09-05 22:30:49 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+my_string = input('Введите произвольное число: ')
+#Подсчет символов
+print(len(my_string))
+#Верхний
+print(my_string.upper())
+#Нижний
+print(my_string.lower())
+#Без пробелов
+print(my_string.replace(' ',''))
+#Только последний символ
+print(my_string [-1])
